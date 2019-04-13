6c9860f42a4771717d9d631e520516f78f8d1268
Jeremias Agustinoy <jeremias_davidenco@hotmail.com>
2019-04-13 20:07:27 -0300

    agregue

--- src/main/java/Buffer.java
+++ src/main/java/Buffer.java
@@ -12,21 +12,17 @@ public class Buffer {
         pila = new LinkedList<Dato>();
     }
 
-    public synchronized void agregar(Dato a){
-        while(pila.size()==colaMax)
-        {
-            try
-            {
+    public synchronized void agregar(){
+        while(pila.size()==colaMax) {
+            try {
                 wait();
-            }
-            catch(InterruptedException e)
-            {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
+            }}
+            Dato a = new Dato();
             pila.add(a);
-            System.out.println("Agregado: "+a.getDato()+ " con espacio ocupado: "+ pila.size());
+            System.out.println("Dato agregado: "+a.getDato()+ " espacio ocupado en pila actualmente: "+ pila.size());
             notify();
-        }
     }
     public synchronized void quitar() {
         while(pila.size()==0)
@@ -38,10 +34,9 @@ public class Buffer {
             catch(InterruptedException e)
             {
                 e.printStackTrace();
-            }
+            }}
             String dato_sacar = pila.poll().toString();
-            System.out.println("Quitado:" + dato_sacar + "con espacio ocupado: " + pila.size());
+            System.out.println("Dato quitado:" + dato_sacar + " espacio ocupado en pila actualmente: " + pila.size());
             notify();
-        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
-public class Main {
+public class Main{
+    public static void Main(){
+        Buffer buffer = new Buffer();
+
+        for(int i=0; i<10; i++){
+            Thread t = new Thread(new Productor(buffer));
+            t.start();
+        }
+
+        for(int j=0; j<2; j++){
+            Thread t2 = new Thread(new Consumidor(buffer));
+            t2.start();
+        }
+    }
 }
