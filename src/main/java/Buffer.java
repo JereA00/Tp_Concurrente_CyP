d7c519b1a1e47fc685c03a52365cc7da9572c4a5
Jeremias Agustinoy <jeremias_davidenco@hotmail.com>
2019-04-13 19:07:45 -0300

    Clase_Buffer

--- src/main/java/Buffer.java
+++ src/main/java/Buffer.java
@@ -1,2 +1,47 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class Buffer {
+    private int colaMax;
+    private Queue<Dato> pila;
+
+    public Buffer()
+    {
+        colaMax=10; //Tamaño de pila
+        /*LinkedList implementa la cola para su funcionalidad y que asi se puedan sacar los archivos de arriba */
+        pila = new LinkedList<Dato>();
+    }
+
+    public synchronized void agregar(Dato a){
+        while(pila.size()==colaMax)
+        {
+            try
+            {
+                wait();
+            }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            pila.add(a);
+            System.out.println("Agregado: "+a.getDato()+ " con espacio ocupado: "+ pila.size());
+            notify();
+        }
+    }
+    public synchronized void quitar() {
+        while(pila.size()==0)
+        {
+            try
+            {
+                wait();
+            }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            String dato_sacar = pila.poll().toString();
+            System.out.println("Quitado:" + dato_sacar + "con espacio ocupado: " + pila.size());
+            notify();
+        }
+    }
 }
